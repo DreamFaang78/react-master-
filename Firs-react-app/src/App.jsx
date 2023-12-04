04ef95c991d313ee6bec4047ca2899b8d0597c94
Agam Singh <faangagam@gmail.com>
2023-12-04 11:22:41 +0530

    Hit This A Demo React app

---
+++ Firs-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hi This is First React App</h1>
+      <p>Hi This Will Be A Jsx File</p>
+      <p>Just A Demo App</p>
+    </>
+  )
+}
+
+export default App
